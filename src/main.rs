d535509bee0d0231e659312d247a16ffbd9e7d4a
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2021-08-03 18:30:25 -0300

    fix typing

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,47 @@ use chrono::prelude::*;
 use colored::*;
 use linux_embedded_hal::{Delay, I2cdev};
 use std::error::Error;
-use std::{thread, time, process};
+use std::{process, thread, time};
 use termion::{clear, cursor};
 
 fn csvwriter() -> Result<(), Box<dyn Error>> {
-        let mut bme280 = BME280::new_primary(I2cdev::new("/dev/i2c-1").unwrap(), Delay);
-        let mut wtr = csv::Writer::from_path("sensor.csv")?;
-        bme280.init().unwrap();
-        wtr.write_record(&["Dia", "Hora", "Temperatura", "Humidade", "Pressão"])?;
-        loop {
+    let mut bme280 = BME280::new_primary(I2cdev::new("/dev/i2c-1").unwrap(), Delay);
+    let mut wtr = csv::Writer::from_path("sensor.csv")?;
+    bme280.init().unwrap();
+    wtr.write_record(&["Dia", "Hora", "Temperatura", "Humidade", "Pressão"])?;
+    loop {
         let measurements = bme280.measure().unwrap();
 
-            let now = Local::now();
+        let now = Local::now();
 
-            let time = format!("{:02}:{:02}:{:02}", now.hour(), now.minute(), now.second());
-            let day = format!("{:02}/{:02}/{:04}", now.day(), now.month(), now.year());
-            let humidity = format!("{:.2}", measurements.humidity);
-            let temp = format!("{:.2}", measurements.temperature);
-            let _press = format!("{:.2}", (measurements.pressure / 1000 as f32));
-            wtr.serialize((day.to_string(), time.to_string(), temp, humidity, _press))?;
-            wtr.flush()?;
-            thread::sleep(time::Duration::from_secs(60));
-        }
+        let time = format!("{:02}:{:02}:{:02}", now.hour(), now.minute(), now.second());
+        let day = format!("{:02}/{:02}/{:04}", now.day(), now.month(), now.year());
+        let humidity = format!("{:.2}", measurements.humidity);
+        let temp = format!("{:.2}", measurements.temperature);
+        let _press = format!("{:.2}", (measurements.pressure / 1000 as f32));
+        wtr.serialize((day.to_string(), time.to_string(), temp, humidity, _press))?;
+        wtr.flush()?;
+        thread::sleep(time::Duration::from_secs(60));
+    }
 }
 
 fn main() {
-
-    // First initialize the sensor and CSV writer
+    // First initialize the sensor
     let mut bme280 = BME280::new_primary(I2cdev::new("/dev/i2c-1").unwrap(), Delay);
     bme280.init().unwrap();
+
+    // Write sensor data to csv every 1 minute in a separate thread
     thread::spawn(|| {
-            if let Err(err) = csvwriter() {
-        println!("{}", err);
-        process::exit(1);
+        if let Err(err) = csvwriter() {
+            println!("{}", err);
+            process::exit(1);
         }
     });
+
+    // Clear screen
     print!("{}{}", clear::All, cursor::Goto(1, 1));
+
+    // Print sensor data to screen every second forever
     loop {
         let measurements = bme280.measure().unwrap();
 
@@ -68,5 +73,5 @@ fn main() {
         );
         thread::sleep(time::Duration::from_secs(1));
         print!("{}", clear::All);
-    };
+    }
 }
