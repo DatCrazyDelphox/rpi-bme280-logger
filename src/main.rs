41a30ec9ef1700e1d7b95fbdd791aa53e37f36e6
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2021-08-03 21:24:21 -0300

    rewrote logic to fix race condition

--- src/main.rs
+++ src/main.rs
@@ -3,48 +3,24 @@ use chrono::prelude::*;
 use colored::*;
 use linux_embedded_hal::{Delay, I2cdev};
 use std::error::Error;
-use std::{process, thread, time};
+use std::{thread, time};
 use termion::{clear, cursor};
 
-fn csvwriter() -> Result<(), Box<dyn Error>> {
-    let mut bme280 = BME280::new_primary(I2cdev::new("/dev/i2c-1").unwrap(), Delay);
-    let mut wtr = csv::Writer::from_path("sensor.csv")?;
-    bme280.init().unwrap();
-    wtr.write_record(&["Dia", "Hora", "Temperatura", "Humidade", "Pressão"])?;
-    loop {
-        let measurements = bme280.measure().unwrap();
-
-        let now = Local::now();
-
-        let time = format!("{:02}:{:02}:{:02}", now.hour(), now.minute(), now.second());
-        let day = format!("{:02}/{:02}/{:04}", now.day(), now.month(), now.year());
-        let humidity = format!("{:.2}", measurements.humidity);
-        let temp = format!("{:.2}", measurements.temperature);
-        let _press = format!("{:.2}", (measurements.pressure / 1000 as f32));
-        wtr.serialize((day.to_string(), time.to_string(), temp, humidity, _press))?;
-        wtr.flush()?;
-        thread::sleep(time::Duration::from_secs(60));
-    }
-}
-
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // First initialize the sensor
     let mut bme280 = BME280::new_primary(I2cdev::new("/dev/i2c-1").unwrap(), Delay);
     bme280.init().unwrap();
-
-    // Write sensor data to csv every 1 minute in a separate thread
-    thread::spawn(|| {
-        if let Err(err) = csvwriter() {
-            println!("{}", err);
-            process::exit(1);
-        }
-    });
+    let mut wtr = csv::Writer::from_path("sensor.csv")?;
+    wtr.write_record(&["Dia", "Hora", "Temperatura", "Humidade", "Pressão"])?;
 
     // Clear screen
     print!("{}{}", clear::All, cursor::Goto(1, 1));
 
     // Print sensor data to screen every second forever
     loop {
+        let mut time_count = 0;
+        time_count += 1;
+
         let measurements = bme280.measure().unwrap();
 
         let now = Local::now();
@@ -54,6 +30,7 @@ fn main() {
         let humidity = format!("{:.2}", measurements.humidity);
         let temp = format!("{:.2}", measurements.temperature);
         let _press = format!("{:.2}", (measurements.pressure / 1000 as f32));
+
 
         println!("{} - {}", day.white().bold(), time.white().bold());
         println!(
@@ -71,7 +48,13 @@ fn main() {
             _press.bright_green().bold(),
             "KPa".bright_green().bold()
         );
+
+        if time_count == 60 {
+            wtr.serialize((day.to_string(), time.to_string(), temp, humidity, _press))?;
+            wtr.flush()?;
+        }
+
         thread::sleep(time::Duration::from_secs(1));
-        print!("{}", clear::All);
+        print!("{}{}", clear::All, cursor::Goto(1, 1));
     }
 }
