b5031341ca050b4fb120349c5a366679e3e8154a
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2021-08-04 09:53:59 -0300

    use consts for better memory handling

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,22 @@ use std::error::Error;
 use std::{thread, time};
 use termion::{clear, cursor};
 
+// Main Variables
+const I2C: &str = "/dev/i2c-1";
+const FILEPATH: &str = "./sensor.csv";
+const INTERVAL: u16 = 60; // In seconds
+
 fn main() -> Result<(), Box<dyn Error>> {
     // First initialize the sensor
-    let mut bme280 = BME280::new_primary(I2cdev::new("/dev/i2c-1").unwrap(), Delay);
+    let mut bme280 = BME280::new_primary(I2cdev::new(I2C).unwrap(), Delay);
     bme280.init().unwrap();
-    let mut wtr = csv::Writer::from_path("sensor.csv")?;
+    let mut wtr = csv::Writer::from_path(FILEPATH)?;
     wtr.write_record(&["Dia", "Hora", "Temperatura", "Humidade", "Pressão"])?;
 
     // Clear screen
     print!("{}{}", clear::All, cursor::Goto(1, 1));
 
-    let mut trigger: u8 = 61;
+    let mut trigger: u16 = INTERVAL + 1;
     // Print sensor data to screen every second forever
     loop {
         let measurements = bme280.measure().unwrap();
@@ -28,7 +33,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         let humidity = format!("{:.2}", measurements.humidity);
         let temp = format!("{:.2}", measurements.temperature);
         let _press = format!("{:.7}", (measurements.pressure / 1000 as f32));
-
 
         println!("{} - {}", day.white().bold(), time.white().bold());
         println!(
@@ -47,10 +51,14 @@ fn main() -> Result<(), Box<dyn Error>> {
             "kPa".bright_green().bold()
         );
 
-        if trigger == 60 || trigger == 61 {
+        if trigger == INTERVAL || trigger == INTERVAL + 1 {
             wtr.serialize((day.to_string(), time.to_string(), temp, humidity, _press))?;
             wtr.flush()?;
-            println!("{}{}","Wrote to csv at ".white().bold(), time.bright_green().bold());
+            println!(
+                "{}{}",
+                "Wrote to csv at ".white().bold(),
+                time.bright_green().bold()
+            );
             trigger = 0;
         }
 
@@ -58,6 +66,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("Trigger: {}", trigger);
 
         thread::sleep(time::Duration::from_secs(1));
+
         print!("{}{}", clear::All, cursor::Goto(1, 1));
     }
 }
